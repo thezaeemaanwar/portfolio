2e0a37a891cbf7a83e591cb1e65974a891b924fc
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-09-09 00:24:15 +0500

    🙇‍♀️ INITIAL Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: "#ffd428",
+        grey: "#4e586e",
+        white: "#ffffff",
+        pink: "#f54b64",
+        orange: "#f78361",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
