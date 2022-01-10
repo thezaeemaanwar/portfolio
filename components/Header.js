7c7cbf5a7b9d0098b042570a66358ffdc2769042
Zaeema Anwar <thezaeemaanwar@gmail.com>
2022-01-10 23:38:34 +0500

    📦 NEW: Links in Header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="h-14 w-full sm:w-10/12 flex border-b-10 border-grey items-center justify-between">
-        <div>thezaeemaanwar</div>
+      <div className="h-14 w-full sm:w-10/12 flex border-b-10 border-grey items-center justify-between hover:cursor-pointer">
+        <Link href="/" passHref>
+          <div>thezaeemaanwar</div>
+        </Link>
         <ul className="flex w-full sm:w-3/12 justify-between">
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <Link href="About" passHref>
+            <li className="hover:cursor-pointer">About</li>
+          </Link>
+          <Link href="Projects" passHref>
+            <li className="hover:cursor-pointer">Projects</li>
+          </Link>
+          <Link href="Contact" passHref>
+            <li className="hover:cursor-pointer">Contact</li>
+          </Link>
         </ul>
       </div>
     </div>
